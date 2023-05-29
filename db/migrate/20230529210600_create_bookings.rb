42b280975c4efe5b8850be0fcbd4e332409604ea
loic poosing <poosingloic@gmail.com>
2023-05-29 23:17:22 +0200

    migration booking model

--- db/migrate/20230529210600_create_bookings.rb
+++ db/migrate/20230529210600_create_bookings.rb
@@ -4,7 +4,7 @@ class CreateBookings < ActiveRecord::Migration[7.0]
       t.integer :start_month
       t.boolean :status
       t.references :user, null: false, foreign_key: true
-      t.references :spaceship, null: false, foreign_key: true
+      t.references :starship, null: false, foreign_key: true
 
       t.timestamps
     end
