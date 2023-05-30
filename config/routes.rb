3c777cd892da16b2268423107ffbd200c3bfec81
loic poosing <poosingloic@gmail.com>
2023-05-30 16:21:34 +0200

    booking route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  resources :starships do
+  resources :users do
     resources :bookings, only: [:index, :new, :create]
   end
   resources :bookings, only: [:destroy]
+  resources :starships
 end
