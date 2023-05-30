9935faa9a221ea9b00543dc4da7ee3a9d34eaa7a
loic poosing <poosingloic@gmail.com>
2023-05-30 18:10:27 +0200

    routes & controller booking + change status

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,5 +1,5 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :starship
-
+  enum status: {0: "en cours", 1: "accepte", 2: "refuse"}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_29_210600) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_30_155943) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "bookings", force: :cascade do |t|
     t.integer "start_month"
-    t.boolean "status"
     t.bigint "user_id", null: false
     t.bigint "starship_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "status"
     t.index ["starship_id"], name: "index_bookings_on_starship_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
