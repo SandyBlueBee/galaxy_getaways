76baa43063166b5e9b37b72bcf83642125856e37
Sandra Yukna <102870351+SandyBlueBee@users.noreply.github.com>
2023-05-31 10:33:37 +0200

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,13 +5,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-Starship.destroy_all
 
-Starship.create!(name: "Millenium Falcon", model: "T-34", manufacturer: "Corellia", hyperdrive_rating: 2, price: 180000, max_speed: 200, passangers: 1, length: 10, location: "Corellia", user_id: 1)
-Starship.create!(name: "Desol", model: "T-34", manufacturer: "Corellia", hyperdrive_rating: 2, price: 180000, max_speed: 200, passangers: 1, length: 10, location: "Corellia", user_id: 1)
-
-Starship.create(
-  name: "etoile rouge", model: "oui", starship_class: "t2", manufacturer: "ford", length: "55",
-  passangers: "3", max_speed: "8", hyperdrive_rating: "4", price: "5", location: "tatouin", user_id: "1"
-)
 # Booking.create(start_month: "4", status: true)
