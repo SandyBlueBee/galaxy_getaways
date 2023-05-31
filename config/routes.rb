8e3924b20c196e04a8e23ce7ad151af40b4da84f
loic poosing <poosingloic@gmail.com>
2023-05-31 13:30:22 +0200

    booking route index de-nested

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   resources :starships do
-    resources :bookings, only: [:index, :new, :create]
+    resources :bookings, only: [:new, :create]
   end
-  resources :bookings, only: [:destroy]
+  resources :bookings, only: [:destroy, :index]
 end
